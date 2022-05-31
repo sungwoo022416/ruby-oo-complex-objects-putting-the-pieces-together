2fc395f353f573164c8d88c767ebead503f34b7f
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-05-31 08:22:58 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
 class Book
-    attr_accessor :title, :author, :page_count, :genre
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+    
     def initialize(title)
         @title = title
-        @author = author
-        @page_count = page_count
-        @genre = genre
     end
 
     def turn_page
