ca744cb122c4f29f606fcf0eefafbb8169cb51a3
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-05-31 08:13:30 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+require 'pry'
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
